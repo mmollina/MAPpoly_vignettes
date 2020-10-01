e9421b51a0fed732565c0f5f6c3c820b91b61bd2
Marcelo Mollinari <mmollina@GMAIL.COM>
2020-10-01 16:00:39 -0300

    VCF management

--- misc_scripts/VCF_subset/organizing_data.sh
+++ misc_scripts/VCF_subset/organizing_data.sh
@@ -2,10 +2,10 @@
 #write.table(data.frame("x" = dat$mrk.names[seq(1, dat$n.mrk, length.out = 100)]), file = "misc_scripts/VCF_subset/SNPs_ch12.txt", quote = F, row.names = F, col.names = F)
 
 vcftools --vcf ~/repos/BT_map/data/vcf_sweetpotato_original/myGBSGenos_trifida_mergedSNPs_mergedTaxa_chr3.vcf --snps SNPs_ch3.txt --recode --recode-INFO-all --out selected_SNPs_ch3.vcf
-vcftools --vcf ~/repos/BT_map/data/vcf_sweetpotato_original/myGBSGenos_trifida_mergedSNPs_mergedTaxa_chr12.vcf --snps SNPs_ch3.txt --recode --recode-INFO-all --out selected_SNPs_ch12.vcf
+vcftools --vcf ~/repos/BT_map/data/vcf_sweetpotato_original/myGBSGenos_trifida_mergedSNPs_mergedTaxa_chr12.vcf --snps SNPs_ch12.txt --recode --recode-INFO-all --out selected_SNPs_ch12.vcf
 
-python VCF2SM.py -i ~/repos/ESALQ_course/data/myGBSGenos_trifida_mergedSNPs_mergedTaxa_chr3.vcf -o trifida_chr3.vcf -d 20 -a AD -g BT -1 Beauregard_BT -2 Tanzania_BT -S ./src/SuperMASSA.py -I f1 -M 2:6 -f 2:6 -p 0.80 -n 0.50 -c 0.50 -t 8 &
-python VCF2SM.py -i ~/repos/ESALQ_course/data/myGBSGenos_trifida_mergedSNPs_mergedTaxa_chr12.vcf -o trifida_chr12.vcf -d 20 -a AD -g BT -1 Beauregard_BT -2 Tanzania_BT -S ./src/SuperMASSA.py -I f1 -M 2:6 -f 2:6 -p 0.80 -n 0.50 -c 0.50 -t 8 &
+python VCF2SM/VCF2SM.py -i ~/repos/BT_map/data/vcf_sweetpotato_original/myGBSGenos_trifida_mergedSNPs_mergedTaxa_chr3.vcf -o trifida_chr3.vcf -d 20 -a AD -g BT -1 Beauregard_BT -2 Tanzania_BT -S ./src/SuperMASSA.py -I f1 -M 2:6 -f 2:6 -p 0.80 -n 0.50 -c 0.50 -t 8 &
+python VCF2SM/VCF2SM.py -i ~/repos/BT_map/data/vcf_sweetpotato_original/myGBSGenos_trifida_mergedSNPs_mergedTaxa_chr12.vcf -o trifida_chr12.vcf -d 20 -a AD -g BT -1 Beauregard_BT -2 Tanzania_BT -S ./src/SuperMASSA.py -I f1 -M 2:6 -f 2:6 -p 0.80 -n 0.50 -c 0.50 -t 8 &
 
 vcf-sort trifida_chr3.vcf > sorted_trifida_chr3.vcf 
 vcf-sort trifida_chr12.vcf > sorted_trifida_chr12.vcf 
@@ -18,3 +18,5 @@ tabix -p vcf sorted_trifida_chr12.vcf.gz
 
 vcf-merge sorted_trifida_chr3.vcf.gz sorted_trifida_chr12.vcf.gz > trifida_ch3_chr12.vcf
 bgzip trifida_ch3_chr12.vcf
+
+vcf-sort trifida_chr3.vcf > sorted_trifida_chr3.vcf 
