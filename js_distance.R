809fa9115b3b420761587e84e1365698e8849a9e
Marcelo Mollinari <mmollina@GMAIL.COM>
2020-10-03 19:27:50 -0300

    rcpp

---
+++ js_distance.R
@@ -0,0 +1,15 @@
+js_distance <- function(mat) {
+  kld = function(p,q) sum(ifelse(p == 0 | q == 0, 0, log(p/q)*p))
+  res = matrix(0, nrow(mat), nrow(mat))
+  for (i in 1:(nrow(mat) - 1)) {
+    for (j in (i+1):nrow(mat)) {
+      m = (mat[i,] + mat[j,])/2
+      d1 = kld(mat[i,], m)
+      d2 = kld(mat[j,], m)
+      res[j,i] = sqrt(.5*(d1 + d2))
+    }
+  }
+  res
+}
+mat<-matrix(1:100, 10,10)
+js_distance(mat)
